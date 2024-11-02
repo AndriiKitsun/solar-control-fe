8a5b6aff8b752ae84fa742baadcc723e784dbc0f
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2024-11-02 15:33:46 +0200

    Implement main container and footer

--- src/app/layout/components/layout/layout.component.ts
+++ src/app/layout/components/layout/layout.component.ts
@@ -4,6 +4,10 @@ import { FooterComponent } from '../footer/footer.component';
 import { MenuComponent } from '../menu/menu.component';
 import { Button } from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
+import { LayoutService } from '../../services/layout/layout.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-layout',
@@ -14,8 +18,17 @@ import { RouterOutlet } from '@angular/router';
     MenuComponent,
     Button,
     RouterOutlet,
+    AsyncPipe,
+    NgIf,
+    CardModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.less',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  constructor(private readonly layoutService: LayoutService) {}
+
+  get isMenuOpened$(): Observable<boolean> {
+    return this.layoutService.isMenuOpened$;
+  }
+}
