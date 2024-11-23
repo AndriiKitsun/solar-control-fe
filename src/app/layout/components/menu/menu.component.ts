440ca168b70c37478acd21a50f3963b57e23467f
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2024-11-23 11:26:24 +0200

    kek

--- src/app/layout/components/menu/menu.component.ts
+++ src/app/layout/components/menu/menu.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MenuItemComponent, Menu],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  items: MenuItem[] = [
+    {
+      label: 'label1',
+    },
+    {
+      label: 'label2',
+    },
+    {
+      label: 'not visible',
+      visible: false,
+    },
+    {
+      label: 'label3',
+    },
+  ];
+}
