6687da63be7cab2525b6a38cd3e8a266e7cd64f5
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-01 15:51:39 +0000

    feature/SC-40-cd-pipeline: https://github.com/AndriiKitsun/solar-control-fe/actions/runs/12106916370
    Triggered by commit: https://github.com/AndriiKitsun/solar-control-fe/commit/97bcfb0c5dd6e2d3fb348f1ceca3b5c41f6c6b4f

--- chunk-HNM6ILJN.js
+++
@@ -1,1 +0,0 @@
-import{Sa as o,Ta as s,U as t,db as i,jb as r}from"./chunk-7TTOVAOP.js";var a=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=t({type:n,selectors:[["app-miners"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),i(1,"miners works! 123"),s())}})};export{a as MinersComponent};
