1f843613a67f9d7e664925ae9bc508dea3a0b687
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-07 12:00:52 +0000

    https://github.com/AndriiKitsun/solar-control-fe/actions/runs/12212674161
    Triggered by commit: https://github.com/AndriiKitsun/solar-control-fe/commit/431285f541fcb3ea284c5e8ac633404aa929c4d8

--- chunk-J2UATFNG.js
+++
@@ -1,1 +0,0 @@
-import{V as n,Za as o,_a as a,lb as i,rb as s}from"./chunk-NA7UN6BR.js";var p=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-station"]],standalone:!0,features:[s],decls:2,vars:0,template:function(t,r){t&1&&(o(0,"p"),i(1,"station works!"),a())}})};export{p as StationComponent};
