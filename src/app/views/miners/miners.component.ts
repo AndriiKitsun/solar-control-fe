9562011c21c56a2665ef90922e82fc984fbdef74
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2024-12-08 18:15:28 +0200

    Delete redundant imports

--- src/app/views/miners/miners.component.ts
+++ src/app/views/miners/miners.component.ts
@@ -1,34 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuComponent } from '@layout/components/menu/menu.component';
-import { Menu } from 'primeng/menu';
-import { Badge } from 'primeng/badge';
-import { Avatar } from 'primeng/avatar';
-import { NgIf, AsyncPipe } from '@angular/common';
-import { Ripple } from 'primeng/ripple';
-import { Button } from 'primeng/button';
-import { ButtonGroup } from 'primeng/buttongroup';
-import { RouterLinkActive } from '@angular/router';
 import { MinerService } from './services';
 import { map, Observable } from 'rxjs';
 import { MinerType } from './models';
 import { MINER_MENU_ITEM_MAPPING } from './constants';
 import { MinerMenuItem } from './types';
+import { AsyncPipe } from '@angular/common';
+import { Menu } from 'primeng/menu';
+import { Ripple } from 'primeng/ripple';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-miners',
   standalone: true,
-  imports: [
-    MenuComponent,
-    Menu,
-    Badge,
-    Avatar,
-    NgIf,
-    Ripple,
-    Button,
-    ButtonGroup,
-    AsyncPipe,
-    RouterLinkActive,
-  ],
+  imports: [AsyncPipe, Menu, Ripple, Button],
   templateUrl: './miners.component.html',
   styleUrl: './miners.component.scss',
 })
