543d61671ecc7a2cf838d9c3979309d06503e92c
Andrii Kitsun <andrii.kitsun@gmail.com>
2024-12-08 16:17:33 +0000

    https://github.com/AndriiKitsun/solar-control-fe/actions/runs/12223356037
    Triggered by commit: https://github.com/AndriiKitsun/solar-control-fe/commit/637e9bdf753c8811e8d65cc7af0e4e50fa6f08b0

---
+++ chunk-PRMK453B.js
@@ -0,0 +1,1 @@
+import"./chunk-BYXBJQAS.js";import"./chunk-7CGTOI24.js";var e=[{path:"",loadComponent:()=>import("./chunk-7UKUXKRH.js").then(o=>o.DashboardComponent)},{path:"miners",loadComponent:()=>import("./chunk-DJE3RFCH.js").then(o=>o.MinersComponent)},{path:"stations",loadComponent:()=>import("./chunk-LPF5QK6N.js").then(o=>o.StationsComponent)}];export{e as default};
