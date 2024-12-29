589cf9bd80fd96d495228d435c78e321a4214ded
Andrii Kitsun <Andrii.Kitsun@gmail.com>
2024-12-29 16:36:24 +0200

    fix unit tests. again

--- src/app/views/sensors/services/sensors/sensor.service.spec.ts
+++ src/app/views/sensors/services/sensors/sensor.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { SensorsService } from './sensors.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('SensorService', () => {
   let service: SensorsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(SensorsService);
   });
 
