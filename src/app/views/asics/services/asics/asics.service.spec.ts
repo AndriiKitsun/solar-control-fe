f24511610d582d171c7088ca6c4c98f2c5355438
Andrii Kitsun <andrii.kitsun@gmail.com>
2025-01-17 18:26:06 +0200

    SC-71: [Asics] Integrate sidebar with BE endpoint (#28)

--- src/app/views/asics/services/asics/asics.service.spec.ts
+++ src/app/views/asics/services/asics/asics.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AsicsService } from './asics.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AsicsService', () => {
   let service: AsicsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(AsicsService);
   });
 
--- src/app/views/asics/services/asics/asics.service.ts
+++ src/app/views/asics/services/asics/asics.service.ts
@@ -1,42 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, delay } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AsicModel } from '../../asics.models';
-import { FakeDataHelper } from '@common/helpers/fake-data.helper';
+import { HttpClient } from '@angular/common/http';
+import { env } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AsicsService {
-  private readonly minerNames: string[] = [
-    'Asic Bitmain S19 110 Th/s',
-    'Asic Bitmain L9 16 Gh/s',
-    'Asic Bitmain L7 9050 Mh/s',
-    'Asic Bitmain KA3 166 Th/s',
-    'Jasminer X44-P 23.4 Gh/s',
-    'IceRiver KAS KS5M 15Th',
-  ];
-  private readonly addresses: string[] = [
-    'home',
-    'home2',
-    'home3',
-    'father',
-    'mother',
-    'brother',
-    'address1',
-    'address2',
-    'address3',
-  ];
+  constructor(private readonly http: HttpClient) {}
 
   getAsics(): Observable<AsicModel[]> {
-    const items = Array.from({ length: 30 }).map<AsicModel>(() => {
-      return {
-        id: FakeDataHelper.randomUUID(),
-        name: FakeDataHelper.randomFromList(this.minerNames),
-        address: FakeDataHelper.randomFromList(this.addresses),
-        ip: FakeDataHelper.randomIP(),
-      };
-    });
-
-    return of(items).pipe(delay(0));
+    return this.http.get<AsicModel[]>(`${env.apiEndpoint}/asics`);
   }
 }
